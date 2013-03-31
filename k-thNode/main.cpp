bffe2db6b69bdaa0ccd1e85cb35411758388d8be
Cong <Cong@CongmatoiMac.local>
2013-03-31 19:47:11 +0800

    k-thNode

--- k-thNode/main.cpp
+++ k-thNode/main.cpp
@@ -7,12 +7,39 @@
 //
 
 #include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+
+
+Node* findKthToTail(Node* phead, int k){
+    if(k == 0 || phead == NULL)
+        return NULL;
+
+    Node* ptr1 = phead;
+    Node* ptr2 = phead;
+    int n = k - 1;
+    while(n--){
+        if(!ptr1->next){
+            cout<<"the length of link is less than k!"<<endl;
+            return NULL;
+        }
+        ptr1 = ptr1->next;
+    }
+    while(ptr1->next){
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+    return ptr2;
+}
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
     return 0;
 }
 
